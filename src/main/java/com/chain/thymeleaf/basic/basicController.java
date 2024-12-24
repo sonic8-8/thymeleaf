45ee5efe261fc9550c5a44268cedd92c0aa3356b
이승환 <seaung13@gmail.com>
2024-12-25 03:27:28 +0900

    feat: URL 링크

--- src/main/java/com/chain/thymeleaf/basic/basicController.java
+++ src/main/java/com/chain/thymeleaf/basic/basicController.java
@@ -84,5 +84,12 @@ public class basicController {
         model.addAttribute("localDateTime", LocalDateTime.now());
         return "basic/date";
     }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
 }
 
