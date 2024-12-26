1f21be295cfa1b30b0f4ad5e7bb0eae550319f23
이승환 <seaung13@gmail.com>
2024-12-26 20:33:09 +0900

    feat: 조건부 평가 - th:if, th:unless, th:switch, th:case

--- src/main/java/com/chain/thymeleaf/basic/basicController.java
+++ src/main/java/com/chain/thymeleaf/basic/basicController.java
@@ -112,15 +112,23 @@ public class basicController {
     
     @GetMapping("/each")
     public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    private static void addUsers(Model model) {
         List<User> users = new ArrayList<>();
         users.add(new User("userA", 10));
         users.add(new User("userB", 20));
         users.add(new User("userC", 30));
         model.addAttribute("users", users);
-        
-        return "basic/each";
     }
 
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
 
 }
 
